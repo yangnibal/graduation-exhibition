2be54d451e768315e8981933bfa3f6cf60915712
GangbacOl <gangyeonjo336@gmail.com>
2021-04-19 14:01:03 +0900

    update: 전역 스타일 시트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Routes from './pages'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Routes from './pages';
+import './styles/index.css';
 
-function App(){
-	return(
-		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" component={Routes.Home}/>
-				<Route exact path="/works" component={Routes.Works}/>
-				<Route exact path="/contact" component={Routes.Contact}/>
-			</Switch>
-		</BrowserRouter>
-	)
+function App() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Routes.Home} />
+                <Route exact path="/works" component={Routes.Works} />
+                <Route exact path="/contact" component={Routes.Contact} />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
