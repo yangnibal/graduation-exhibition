3e775dc0db8c16ed1a64545c9f60f69b1b6a944d
GangbacOl <gangyeonjo336@gmail.com>
2021-04-20 10:51:26 +0900

    update: 모바일 분기 로직 작성

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import Footer from '../Footer'
-import Header from '../Header'
+import React from 'react';
+
+import Footer from '../Footer';
+import Header from '../Header';
+import Tabbar from '../Tabbar';
 
 interface Props {
-    type: 'HOME' | 'WORKS' | 'CONTACT'
+    type: 'HOME' | 'WORKS' | 'CONTACT';
+    isMobile: RegExpMatchArray | null;
 }
 
-const Layout: React.FC<Props> = ({children, type}) => {
-    return(
+const Layout: React.FC<Props> = ({ children, type, isMobile }) => {
+    return (
         <div style={{ width: '100%', minHeight: '100vh' }}>
-            <Header type={type}/>
+            {(type !== 'WORKS' || (type === 'WORKS' && !isMobile)) && (
+                <Header type={type} isMobile={isMobile} />
+            )}
+            {isMobile && <Tabbar type={type} />}
             {children}
-            <Footer/>
+            <Footer />
         </div>
-    )
-}
+    );
+};
 
-export default Layout
+export default Layout;
