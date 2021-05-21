0d28585a7c0df25503be167f37827966c7118d68
GangbacOl <gangyeonjo336@gmail.com>
2021-05-21 21:59:11 +0900

    feat: Add content route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Routes from './pages';
+import Content from './pages/Works/Detail/';
 import './styles/index.css';
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={Routes.Home} />
                 <Route exact path="/works" component={Routes.Works} />
+                <Route exact path="/works/:id" component={Content} />
                 <Route exact path="/contact" component={Routes.Contact} />
             </Switch>
         </BrowserRouter>
